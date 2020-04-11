f5403686c6061145892b8c25402efc77fc50a763
SankarBhagawanPalla <54730150+SankarBhagawanPalla@users.noreply.github.com>
2020-04-11 11:43:57 -0400

    Creating Singleton RestTemplate Bean.

--- movie-catalog-service/src/main/java/com/springworks/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/springworks/moviecatalogservice/resources/MovieCatalogResource.java
@@ -3,6 +3,7 @@ package com.springworks.moviecatalogservice.resources;
 import com.springworks.moviecatalogservice.models.CatalogItem;
 import com.springworks.moviecatalogservice.models.Movie;
 import com.springworks.moviecatalogservice.models.Rating;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,13 +16,11 @@ import java.util.stream.Collectors;
 @RequestMapping("/catalog")
 public class MovieCatalogResource {
 
-
-
+    @Autowired
+    private RestTemplate restTemplate;
 
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
-
-        RestTemplate restTemplate = new RestTemplate();
 
         List<Rating> ratings = Arrays.asList(
                 new Rating("679", 4),
