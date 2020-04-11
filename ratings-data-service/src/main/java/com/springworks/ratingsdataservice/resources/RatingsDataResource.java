b599963f05469a12d728268d65adec4c11954981
SankarBhagawanPalla <54730150+SankarBhagawanPalla@users.noreply.github.com>
2020-04-11 12:49:17 -0400

    Developed API endpoint to provide user ratings.

--- ratings-data-service/src/main/java/com/springworks/ratingsdataservice/resources/RatingsDataResource.java
+++ ratings-data-service/src/main/java/com/springworks/ratingsdataservice/resources/RatingsDataResource.java
@@ -1,21 +1,29 @@
 package com.springworks.ratingsdataservice.resources;
 
 import com.springworks.ratingsdataservice.models.Rating;
+import com.springworks.ratingsdataservice.models.UserRating;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequestMapping("/ratingsdata")
 public class RatingsDataResource {
 
-    @RequestMapping("/{movieId}")
-    public Rating getRating(@PathVariable("movieId") String movieId){
+    @RequestMapping("/users/{userId}")
+    public UserRating getUserRating(@PathVariable("userId")String userId){
 
-        return new Rating(movieId, 4);
-
+        List<Rating> ratings = Arrays.asList(
+                new Rating("678", 4),
+                new Rating("988", 3)
+        );
+        UserRating userRating = new UserRating();
+        userRating.setUserRating(ratings);
+        return  userRating;
     }
-
 
 }
 
