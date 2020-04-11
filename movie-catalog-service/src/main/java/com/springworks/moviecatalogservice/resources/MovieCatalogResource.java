28c477f7d413ea6513d375c7f91a8d7c06f37a35
SankarBhagawanPalla <54730150+SankarBhagawanPalla@users.noreply.github.com>
2020-04-11 12:50:52 -0400

    Making an API Call to retrieve User ratings.

--- movie-catalog-service/src/main/java/com/springworks/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/springworks/moviecatalogservice/resources/MovieCatalogResource.java
@@ -3,6 +3,7 @@ package com.springworks.moviecatalogservice.resources;
 import com.springworks.moviecatalogservice.models.CatalogItem;
 import com.springworks.moviecatalogservice.models.Movie;
 import com.springworks.moviecatalogservice.models.Rating;
+import com.springworks.moviecatalogservice.models.UserRating;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,24 +27,21 @@ public class MovieCatalogResource {
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
 
-        List<Rating> ratings = Arrays.asList(
-                new Rating("679", 4),
-                new Rating("998", 3)
-        );
+       UserRating ratings = restTemplate.getForObject("http://localhost:8082/ratingsdata/users/"+userId, UserRating.class);
 
-        return ratings.stream().map(rating -> {
-
-            //Movie movie = restTemplate.getForObject("http://localhost:8081/movies/" + rating.getMovieId(), Movie.class);
-
-            Movie movie = webClientBuilder.build()
-                            .get()
-                            .uri("http://localhost:8081/movies/" + rating.getMovieId())
-                            .retrieve()
-                            .bodyToMono(Movie.class)
-                            .block();
+        return ratings.getUserRating().stream().map(rating -> {
+            //get movie info for each rating
+            Movie movie = restTemplate.getForObject("http://localhost:8081/movies/" + rating.getMovieId(), Movie.class);
+            //put them together
             return  new CatalogItem(movie.getName(), "Test Desc", rating.getRating());
 
         }).collect(Collectors.toList());
 
     }
 }
+/*Movie movie = webClientBuilder.build()
+                            .get()
+                            .uri("http://localhost:8081/movies/" + rating.getMovieId())
+                            .retrieve()
+                            .bodyToMono(Movie.class)
+                            .block();*/
